9051c2bdd45f90081f2dbf200907023dee5ba574
pyradd <mohiuddin.faruq.fb@gmail.com>
2022-06-12 13:32:08 +0200

    changed google provider version

--- examples/cluster/main.tf
+++ examples/cluster/main.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  version = "~> 3.15.0"
+  version = "~> 3.82.0"
 }
 
 provider "google-beta" {
-  version = "~> 3.15.0"
+  version = "~> 3.82.0"
 }
 
 resource "random_id" "suffix" {
@@ -11,13 +11,13 @@ resource "random_id" "suffix" {
 }
 
 locals {
-  creds_path           = "terraform-hmt-2901.json"
+  creds_path = "terraform-hmt-2901.json"
 
   host_project_id      = format("%s-host-%s", var.prefix, random_id.suffix.hex)
   service_x_project_id = format("%s-service-x-%s", var.prefix, random_id.suffix.hex)
   service_y_project_id = format("%s-service-y-%s", var.prefix, random_id.suffix.hex)
 
-  shared_vpc_name      = format("%s-network", var.prefix)
+  shared_vpc_name = format("%s-network", var.prefix)
 
   subnet_x = {
     name   = format("%s-subnet-x", var.prefix)
@@ -33,7 +33,7 @@ locals {
     pod    = "10.58.0.0/16"
     svc    = "10.59.0.0/16"
   }
-  subnets      = [ local.subnet_x, local.subnet_y ]
+  subnets      = [local.subnet_x, local.subnet_y]
   master_x_ips = "10.31.0.0/28"
   master_y_ips = "10.32.0.0/28"
 }
@@ -84,7 +84,7 @@ module "shared_vpc" {
 
 module "gke_x" {
   source = "../../modules/cluster/"
-  
+
   prefix                 = var.prefix
   host_project_id        = module.host_project.project_id
   service_project_id     = module.service_x_project.project_id
@@ -94,17 +94,17 @@ module "gke_x" {
   zones                  = var.region_x_zones
   cluster_name           = "cluster-x"
 
-  network_name           = module.shared_vpc.network_name
-  subnet_name            = local.subnet_x.name
-  master_ipv4_cidr_block = local.master_x_ips
+  network_name               = module.shared_vpc.network_name
+  subnet_name                = local.subnet_x.name
+  master_ipv4_cidr_block     = local.master_x_ips
   master_authorized_networks = var.master_x_authorized_network
-  ip_range_pods          = module.shared_vpc.secondary_ranges[local.subnet_x.name].pod
-  ip_range_services      = module.shared_vpc.secondary_ranges[local.subnet_x.name].svc
-}  
+  ip_range_pods              = module.shared_vpc.secondary_ranges[local.subnet_x.name].pod
+  ip_range_services          = module.shared_vpc.secondary_ranges[local.subnet_x.name].svc
+}
 
 module "gke_y" {
   source = "../../modules/cluster/"
-  
+
   prefix                 = var.prefix
   host_project_id        = module.host_project.project_id
   service_project_id     = module.service_y_project.project_id
@@ -114,10 +114,10 @@ module "gke_y" {
   zones                  = var.region_y_zones
   cluster_name           = "cluster-y"
 
-  network_name           = local.shared_vpc_name
-  subnet_name            = local.subnet_y.name
-  master_ipv4_cidr_block = local.master_y_ips
+  network_name               = local.shared_vpc_name
+  subnet_name                = local.subnet_y.name
+  master_ipv4_cidr_block     = local.master_y_ips
   master_authorized_networks = var.master_y_authorized_network
-  ip_range_pods          = module.shared_vpc.secondary_ranges[local.subnet_y.name].pod
-  ip_range_services      = module.shared_vpc.secondary_ranges[local.subnet_y.name].svc
-}  
+  ip_range_pods              = module.shared_vpc.secondary_ranges[local.subnet_y.name].pod
+  ip_range_services          = module.shared_vpc.secondary_ranges[local.subnet_y.name].svc
+}
